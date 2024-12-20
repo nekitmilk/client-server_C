ff75b2ad38bd14af9ddf53de98cd19caac2e24cd
tieshagr <tieshagr@student.21>
2024-12-20 15:39:31 +0300

    its work 2

--- src/logger/logger.c
+++ src/logger/logger.c
@@ -34,29 +34,51 @@ void *write_log(void *arg) {
     char *file = (strcmp(name, NAME1) == 0) ? FILE1 : FILE2;
     int fd = mkfifo(file, 0666);
     if (fd == -1) {
-        perror("mkfifo");
+        //perror("mkfifo");
         fd = open(file, O_RDONLY | O_TRUNC, 777);
-        perror("fd open");
+        //perror("fd open");
         if (fd == -1) {
             return NULL;
         }
     }
     FILE *f = NULL;
     ssize_t bytes = 0;
+    // if ((f = fopen(name, "a")) != NULL) {
+    //     while (1) {
+    //         char buffer[BUFFER_SIZE] = {0};
+    //         bytes = read(fd, buffer, BUFFER_SIZE);
+    //         if (bytes > 0) {
+    //                 // printf("Read %lu bytes\n", bytes);
+    //                 printf("%s\n", buffer);
+    //                 fputs(buffer, f);
+    //         } else if (bytes == -1) {
+    //             printf("Read error\n");
+    //             break;
+    //         } 
+    //     }
+    //     fclose(f);
+    // }
+    // close(fd);
+
     if ((f = fopen(name, "a")) != NULL) {
+        fclose(f);
         while (1) {
             char buffer[BUFFER_SIZE] = {0};
             bytes = read(fd, buffer, BUFFER_SIZE);
             if (bytes > 0) {
                     // printf("Read %lu bytes\n", bytes);
-                    printf("%s\n", buffer);
-                    fputs(buffer, f);
+                    if ((f = fopen(name, "a")) != NULL)
+                    {
+                        printf("%s\n", buffer);
+                        fputs(buffer, f);
+                        fclose(f);
+                    }
+                    
             } else if (bytes == -1) {
                 printf("Read error\n");
                 break;
             } 
         }
-        fclose(f);
     }
     close(fd);
 }
